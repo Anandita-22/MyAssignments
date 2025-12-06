b423b211cc8ac5117df6f67578fa9f16eef39621
Anandita-22 <ananditasridhar22@gmail.com>
2025-12-06 21:56:48 +0530

    Add files via upload
    day1

---
+++ day1/EmployeeDetails.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		
+		String name ="sam";
+		short age=26;
+		float salary =55000.56f;
+		char gender ='M';
+		boolean acceptTerm = true;
+		float rating = 4.5f;
+        long mobileNumber= 1234567891l;
+        int pinCode=600089;
+		char luckyNumber ='7';
+		System.out.println("Employeename"+ " " + name + "\n" + "Employeeage"+ " " +age  +"\n" + "Employeesalary"+" " + salary +"\n" + "Employeegender"+ " " +gender + "\n" +"Employeeterm"+" " + acceptTerm + "\n" +"Employeerating"+" " + rating + "\n" +"EmployeenobileNo"+" " + mobileNumber +"\n" +"EmployeCode"+" " + pinCode);		
+	}
+
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+     float browserVersion=100.2f;
+    String browserName= "firefox";
+    boolean isVisible =true;
+    short releaseYear=1998;
+    char browserLogo='f';
+    System.out.println(browserVersion + "\n" +browserName +"\n" +isVisible +"\n"+releaseYear+"\n"+browserLogo);
+    
+    
+     
+
+	}
+
+}
