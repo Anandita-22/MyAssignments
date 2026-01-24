fb6dc49f2a27764f759b1079236e14fa01baa676
Anandita-22 <ananditasridhar22@gmail.com>
2026-01-24 16:07:38 +0530

    Add files via upload

---
+++ Week2.day1/LearnLocators.java
@@ -0,0 +1,20 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LearnLocators {
+
+	public static void main(String[] args) {
+		ChromeDriver driver= new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("email")).sendKeys("testlead.2023@gmail.com");
+		driver.findElement(By.name("pass")).sendKeys("Tuna@321");
+		driver.findElement(By.name("login")).click();
+		System.out.println(driver.getTitle());
+		
+
+	}
+
+}
